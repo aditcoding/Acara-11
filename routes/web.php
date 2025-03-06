8acc135b48a6bd351b6e2fe63f7a700302aea614
aditcoding <pradanapmaditya@gmail.com>
2025-03-06 11:52:42 +0700

    Menyimpan perubahan sebelum pull

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', [HomeController::class, 'index']);
 
+Route::get('/profile', function () {
+    return 'Ini adalah halaman profile';
+});
+
+Route::get('/dashboard', function () {
+    return 'Ini adalah halaman dashboard';
+});
